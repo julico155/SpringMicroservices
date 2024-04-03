e76388a158e17a8fa101219dc7075c153984dabd
Micaela Roca <106566693+MiCa-ke@users.noreply.github.com>
2024-04-03 05:17:02 -0400

    Bases Actualizadas

---
+++ microservice-categoria/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO categoria (name, descripcion) VALUES ('Cara', 'Productos para la cara');
+INSERT INTO categoria (name, descripcion) VALUES ('Cuerpo', 'Productos para el cuerpo');
+INSERT INTO categoria (name, descripcion) VALUES ('Perfumes', 'Variedad de perfumes');
